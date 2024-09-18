cfcd2fb5e701c97c34df1592668649595b1272ad
jessdobs <jess.dobs@gmail.com>
2024-09-18 16:33:13 -0400

    add new files missed in last commit

---
+++ client/src/components/NotFound.jsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <main>
+      <div className='wrap'>
+        <h2>Not Found</h2>
+        <p>Sorry, the page you requested could not be found.</p>
+      </div>
+    </main>
+  )
+};
+
+export default NotFound;
---
+++ client/src/components/UnhandledError.jsx
@@ -0,0 +1,12 @@
+const UnhandledError = () => {
+  return (
+    <main>
+      <div className='wrap'>
+        <h2>Error</h2>
+        <p>Oops, an unexpected error has occured.</p>
+      </div>
+    </main>
+  )
+};
+
+export default UnhandledError;
